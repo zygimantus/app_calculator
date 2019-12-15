8ff1b434b6eb159de1e97e2859608157548b2499
Zygimantus <zygimantus@gmail.com>
2019-12-15 14:22:34 +0200

    Add: call to the spring API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+
+  state = {
+
+    isLoading: false,
+
+    greeting: ""
+
+  };
+
+  sayHello = async (event) => {
+
+    event.preventDefault();
+
+    let response = await fetch('/greeting?name=' + this.state.greeting);
+
+    let body = await response.json();
+
+    this.setState({ greeting: body.name, isLoading: false, isGreetingVisible: '' });
+
+  }
+
+  updateName = (event) => {
+
+    event.preventDefault();
+
+    this.setState({greeting: event.target.value, isLoading: false});
+
+  }
+
+  render() {
+
+    const {greeting, isLoading} = this.state;
+
+    if (isLoading) {
+
+    return "\n\nLoading...\n\n";
+
+    }
+
+    return (
+
+<div className="App">
+
+<header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<div className="App-intro">
+
+            <input onChange={(event)=>this.updateName(event)} placeholder="Enter Your Name"></input>
+
+            <button onClick={(event)=>this.sayHello(event)}>Please Click Me!</button>
+
+<h2 style={{visibility: this.isGreetingVisible}}>Hello {this.state.greeting}</h2>
+
+        </div>
+
+        </header>
+
     </div>
-  );
+
+    );
+
+  }
+
 }
 
 export default App;
