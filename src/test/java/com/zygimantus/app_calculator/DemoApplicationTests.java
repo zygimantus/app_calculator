704b328cb4ff749a00a70e0b4d204688ac739d33
Zygimantus <zygimantus@gmail.com>
2019-12-15 19:30:46 +0200

    Fix: Unable to find a @SpringBootConfiguration

--- src/test/java/com/zygimantus/app_calculator/DemoApplicationTests.java
+++ src/test/java/com/zygimantus/app_calculator/DemoApplicationTests.java
@@ -1,13 +1,14 @@
 package com.zygimantus.app_calculator;
 
+import com.zygimantus.app.calculator.DemoApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class)
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
