ed5b7c1ab93e945950c8d29c9fc55bc54621ce90
Zygimantus <zygimantus@gmail.com>
2019-12-15 22:38:48 +0200

    Add: test - renders AC button

--- frontend/src/components/App.test.js
+++ frontend/src/components/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './components/App';
+import App from './App';
 
-test('renders learn react link', () => {
+test('renders AC button', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/AC/i);
   expect(linkElement).toBeInTheDocument();
 });
